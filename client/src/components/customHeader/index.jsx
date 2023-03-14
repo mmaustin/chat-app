7c9aafe4c9f10068c70dd6caba0ca8e7a7879d4c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-03-14 10:52:42 -0400

    added conditional chat notification message

--- client/src/components/customHeader/index.jsx
+++ client/src/components/customHeader/index.jsx
@@ -9,7 +9,8 @@ const CustomHeader = ({chat}) => {
         </div>
         <div className='flexbetween'>
             <PhoneIcon className='icon-phone'/>
-            <p className='header-text'>{chat.description}</p>
+            {chat.description !== '⬅️ ⬅️ ⬅️' ? <p className='header-text'>{chat.description}</p> :
+            <p className='header-text'>no chat selected!</p>}
         </div>
     </div>
   )
