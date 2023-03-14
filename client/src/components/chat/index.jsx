69fe75a635411c9c60960b4cc3486593260efdde
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-03-14 10:29:13 -0400

    set up the chat props in chat component

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,8 +1,23 @@
-
+import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from "react-chat-engine-advanced"
+import Header from '@/components/customHeader'
 
 const Chat = () => {
+
+  const chatProps = useMultiChatLogic(
+    import.meta.env.VITE_PROJECT_ID,
+    "rooster",
+    "rooster"
+  )  
+
   return (
-    <div>Chat</div>
+    <div style={{flexBasis: "100%"}}>
+        <MultiChatSocket {...chatProps} />
+        <MultiChatWindow
+            {...chatProps}
+            style={{height: "100vh"}}
+            renderChatHeader={(chat) => <Header chat={chat}/>}
+        />
+    </div>
   )
 }
 export default Chat
