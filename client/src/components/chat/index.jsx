c19de131326a417993337966ff42ba4b60e87586
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-03-15 09:35:44 -0400

    add yet to be created standard message form component to Chat component's return value

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,21 +1,27 @@
 import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from "react-chat-engine-advanced"
 import Header from '@/components/customHeader'
+import StandardMessageForm from '@/components/customMessageForms/StandarMessageForm'
 
 const Chat = () => {
-
+  //chatProps is an object with tons of values, passed down to components below
   const chatProps = useMultiChatLogic(
     import.meta.env.VITE_PROJECT_ID,
     "rooster",
     "rooster"
   )  
-
+   
   return (
     <div style={{flexBasis: "100%"}}>
         <MultiChatSocket {...chatProps} />
         <MultiChatWindow
-            {...chatProps}
-            style={{height: "100vh"}}
-            renderChatHeader={(chat) => <Header chat={chat}/>}
+          {...chatProps}
+          style={{height: "100vh"}}
+          renderChatHeader={(chat) => <Header chat={chat}/>}
+          renderMessageForm={(props) => {
+            return (
+              <StandardMessageForm props={props} activeChat={chatProps.chat} />
+            )
+          }}
         />
     </div>
   )
