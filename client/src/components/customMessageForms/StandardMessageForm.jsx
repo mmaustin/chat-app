0c017568b01212868b1728b11479633557b1d2b1
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-03-15 10:11:10 -0400

    added input with message and message change function on click

--- client/src/components/customMessageForms/StandardMessageForm.jsx
+++ client/src/components/customMessageForms/StandardMessageForm.jsx
@@ -8,6 +8,8 @@ const StandardMessageForm = () => {
     const [attachment, setAttachment] = useState('');
     const [preview, setPreview] = useState('');
 
+    const handleMessageChange = (e) => setMessage(e.target.value);
+
   return (
     <div className="message-form-container">
         {preview && (
@@ -26,6 +28,16 @@ const StandardMessageForm = () => {
                 />
             </div>
         )}
+        <div className="message-form">
+            <div className="message-from-input-container">
+                <input
+                    className="message-form-input"
+                    type="text"
+                    value={message}
+                    onClick={handleMessageChange}
+                />
+            </div>
+        </div>
     </div>
   )
 }
