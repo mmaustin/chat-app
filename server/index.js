361c10852fc386eceb9c6f46ccf5b3cbfc4f8072
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-03-20 10:22:43 -0400

    installed npm openai and configured with api key

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import { Configuration, OpenAIApi }  from 'openai';
 
 dotenv.config();
 const app = express();
@@ -14,6 +15,11 @@ app.use(morgan("common"));
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+export const openai = new OpenAIApi(configuration);
 
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
