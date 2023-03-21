2f0470accf36271538f7a7e1a4e5dba425b74937
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-03-21 10:18:10 -0400

    configured redux store in main jsx file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.scss'
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/dist/query';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import './index.scss';
+import { api } from '@/state/api';
+
+export const store = configureStore({
+    reducer: {[api.reducerPath]: api.reducer},
+    middleware: (getDefault) => getDefault().concat(api.middleware)
+})
+setupListeners(store.dispatch);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 )
