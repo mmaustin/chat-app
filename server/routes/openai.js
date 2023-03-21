68e6c633d7fba9367ccdbe77d55d21b86363edab
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-03-21 09:37:06 -0400

    completed initial set up of openai js file

--- server/routes/openai.js
+++ server/routes/openai.js
@@ -1,4 +1,18 @@
-import { openai } from "../index";
+import { openai } from "../index.js";
 import express from 'express';
 import axios from "axios";
+import dotenv from 'dotenv';
 
+dotenv.config();
+const router = express.Router();
+
+router.post('/text', async(req, res)=> {
+    try {
+        const {text, activeChatId} = req.body;    
+    } catch (error) {
+        console.error('error', error);
+        res.status(500).json({ error: error.message})
+    }
+});
+
+export default router;
