8de1ba07ae00fcdf7ca85f14b5d6d67e499b3f96
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-04-03 11:13:02 -0400

    set up to be implemented use state and post mutations in login/index component

--- client/src/components/login/index.jsx
+++ client/src/components/login/index.jsx
@@ -1,6 +1,22 @@
-
+import { usePostLoginMutation, usePostRegisterMutation } from "@/state/api";
+import { useState, useEffect } from "react";
 
 const Login = () => {
+
+    const [isRegister, setIsRegister] = useState(false);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [triggerLogin, resultLogin] = usePostLoginMutation();
+    const [triggerRegister] = usePostRegisterMutation();
+
+    const handleLogin = () => {
+        triggerLogin({username, password});
+    }
+
+    const handleRegister = () => {
+        triggerRegister({username, password});
+    }
+
   return (
     <div>Login</div>
   )
